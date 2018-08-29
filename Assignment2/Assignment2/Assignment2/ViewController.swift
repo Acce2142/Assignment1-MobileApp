2d56d24ad17a96eda6e51ab01d343b8308f43f55
Duoqi Zheng <zhedy005@mymail.unisa.edu.au>
2018-08-29 15:00:20 +0930

    added metric - imperial conversion

--- Assignment2/Assignment2/Assignment2/ViewController.swift
+++ Assignment2/Assignment2/Assignment2/ViewController.swift
@@ -9,15 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var leverLength: UITextField!
-    @IBOutlet weak var mass: UITextField!
-    @IBOutlet weak var angle: UITextField!
-    @IBOutlet weak var accleration: UITextField!
     @IBOutlet weak var unitSwitch: UISegmentedControl!
     @IBOutlet weak var measurementSwitch: UISegmentedControl!
+    
+    @IBOutlet weak var leverLength: UITextField!
+    var leverLengthDelegate : Double!
+    @IBOutlet weak var mass: UITextField!
+    var massDelegate : Double!
+    @IBOutlet weak var angle: UITextField!
+    var angleDelegate : Double!
+    @IBOutlet weak var accleration: UITextField!
+    var accDelegate : Double!
     @IBOutlet weak var torqueReverseEngineer: UITextField!
+    var torqueReverseEngineerDelegate : Double!
     @IBOutlet weak var massReverseEngineer: UITextField!
+    var massReverseEngineerDelegate : Double!
+    
     @IBOutlet weak var displayArea: UITextView!
     @IBOutlet weak var lengthUnit: UILabel!
     @IBOutlet weak var massUnit: UILabel!
@@ -25,14 +32,111 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         leverLength.delegate? = self;
+        leverLength.text = "2";
         mass.delegate? = self;
+        mass.text = "1000";
         angle.delegate? = self;
+        angle.text = "90";
         accleration.delegate? = self;
+        accleration.text = "5";
         torqueReverseEngineer.delegate? = self;
+        torqueReverseEngineer.text = "10000";
         massReverseEngineer.delegate? = self;
+        massReverseEngineer.text = "1000";
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func metricSwitch(_ sender: Any) {
+        if(measurementSwitch.selectedSegmentIndex == 0){
+            var lengthTemp : Double;
+            var massTemp : Double;
+            var accTemp :Double;
+            lengthTemp = Double(leverLength.text!)!;
+            massTemp = Double(mass.text!)!;
+            accTemp = Double(accleration.text!)!;
+            lengthTemp =  lengthTemp * 0.305;
+            massTemp = massTemp * 0.454;
+            accTemp =  accTemp * 0.304;
+            leverLength.text = String(accTemp.rounded());
+            mass.text = String(massTemp.rounded());
+            accleration.text = String(accTemp.rounded());
+            lengthUnit.text = "Meter";
+            massUnit.text = "kg";
+            accUnit.text = "m/s";
+        } else if(measurementSwitch.selectedSegmentIndex == 1){
+            var lengthTemp : Double;
+            var massTemp : Double;
+            var accTemp :Double;
+            lengthTemp = Double(leverLength.text!)!;
+            massTemp = Double(mass.text!)!;
+            accTemp = Double(accleration.text!)!;
+            lengthTemp = lengthTemp * 3.28084;
+            massTemp = massTemp * 2.205;
+            accTemp = accTemp / 0.327;
+            leverLength.text = String(accTemp.rounded());
+            mass.text = String(massTemp.rounded());
+            accleration.text = String(accTemp.rounded());
+            lengthUnit.text = "Feet";
+            massUnit.text = "Pound";
+            accUnit.text = "ft/s";
+        }
+
+    }
+    
+    @IBAction func scaleSwitch(_ sender: Any) {
+        //takes mm and m/s, convert to meter and m/s
+        if(measurementSwitch.selectedSegmentIndex == 0 && unitSwitch.selectedSegmentIndex == 0){
+            var lengthTemp : Double;
+            var accTemp :Double;
+            lengthTemp = Double(leverLength.text!)!;
+            accTemp = Double(accleration.text!)!;
+            lengthTemp = lengthTemp * 1000;
+            accTemp = accTemp * 1000;
+            leverLength.text = String(accTemp.rounded());
+            accleration.text = String(accTemp.rounded());
+            lengthUnit.text = "Meter";
+            accUnit.text = "m/s";
+            //takes meters and m/s. converse to mm and mm/s
+        } else if(measurementSwitch.selectedSegmentIndex == 0 && unitSwitch.selectedSegmentIndex == 1){
+            var lengthTemp : Double;
+            var accTemp :Double;
+            lengthTemp = Double(leverLength.text!)!;
+            accTemp = Double(accleration.text!)!;
+            lengthTemp = lengthTemp / 1000;
+            accTemp = accTemp / 1000;
+            leverLength.text = String(accTemp.rounded());
+            accleration.text = String(accTemp.rounded());
+            lengthUnit.text = "Millimeters";
+            accUnit.text = "mm/s";
+            //takes inch, and inch/s. convert to feet, ft/s
+        } else if (measurementSwitch.selectedSegmentIndex == 1 && unitSwitch.selectedSegmentIndex == 0) {
+            var lengthTemp : Double;
+            var accTemp :Double;
+            lengthTemp = Double(leverLength.text!)!;
+            accTemp = Double(accleration.text!)!;
+            lengthTemp = lengthTemp / 1000;
+            accTemp = accTemp / 1000;
+            leverLength.text = String(accTemp.rounded());
+            accleration.text = String(accTemp.rounded());
+            lengthUnit.text = "Feet";
+            accUnit.text = "ft/s";
+            //takes feet, ft/s, convert to inch, inch/s
+        } else if (measurementSwitch.selectedSegmentIndex == 1 && unitSwitch.selectedSegmentIndex == 1){
+            var lengthTemp : Double;
+            var accTemp :Double;
+            lengthTemp = Double(leverLength.text!)!;
+            accTemp = Double(accleration.text!)!;
+            lengthTemp = lengthTemp / 1000;
+            accTemp = accTemp / 1000;
+            leverLength.text = String(accTemp.rounded());
+            accleration.text = String(accTemp.rounded());
+            lengthUnit.text = "Feet";
+            accUnit.text = "ft/s";
+        }
+
+    }
+ 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -56,9 +160,6 @@ class ViewController: UIViewController {
             let torque = (Double(leverLength.text!)! * force) * sin(radius)
             displayArea.text = "Lever length: \(leverLength.text!)m\nMass: \(mass.text!)kg\nAngle: \(angle.text!)°\nAcceleration: \(accleration.text!)m/s\nTorque: \(torque)N"
         }else{
-            lengthUnit.text = "Feet";
-            massUnit.text = "Pound";
-            accUnit.text = "ft/s";
             let radius:Double;
             radius = Double(angle.text!)!*M_PI/180;
             let force = Double(mass.text!)! * Double(accleration.text!)!
